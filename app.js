3e3090fd0c64403b9fba1b693af2c35ce1ca269e
cezszym <cezaryszymanski7@gmail.com>
2020-10-26 15:51:58 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const http = require ('http');
+const express = require('express');
+const app = new express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/',(req, res, next) => {
+    res.sendfile(path.join(__dirname,'./index.html'))
+});
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, err => {
+    if(err){
+        console.log('Cos nie tak');
+    } else {
+        console.log('Server slucha')
+    }
+})
