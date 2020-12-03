8fb062712195bb9d2c72f300b359a72d5c1bb558
cezszym <cezaryszymanski7@gmail.com>
2020-12-03 17:44:58 +0100

    changed port to 5000

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-const http = require ('http');
+const http = require('http');
 const express = require('express');
 const app = new express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/',(req, res, next) => {
-    res.sendfile(path.join(__dirname,'./index.html'))
+app.use('/', (req, res, next) => {
+  res.sendfile(path.join(__dirname, './index.html'));
 });
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-server.listen(port, err => {
-    if(err){
-        console.log('Cos nie tak');
-    } else {
-        console.log('Server slucha')
-    }
-})
+server.listen(port, (err) => {
+  if (err) {
+    console.log('Cos nie tak');
+  } else {
+    console.log('Server slucha');
+  }
+});
